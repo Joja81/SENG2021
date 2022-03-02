e0e6ec6b85710426a40ab17a5d8ab98204b0ccc3
Edward Ambrogio <e.ambrogio@student.unsw.edu.au>
2022-03-02 20:30:40 +1100

    feat: added customerContact function that pulls email and name from ubl formatted xml. + basic tests for the http route & direct tests for the function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from crypt import methods
 import json
+from app import ublExtractor
 from flask import current_app as app, request
 
 @app.route("/", methods = ["GET"])
 def test():
     return json.dumps("Working")
+
+@app.route("/sendinvoice/email", methods = ["POST"])
+def sendInvoiceEmail():
+    XML = request.files.get('file')
+    xml = XML.read()
+    contacts = ublExtractor.customerContact(xml)
+    #email(contacts) waiting for send email implementation
+    return json.dumps("Communication report") #waiting for communication report implementation
