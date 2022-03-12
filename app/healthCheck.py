d50ca31531765a2d722b19b46997821d0211e2af
Michelle Kaminski <z5362642@cse.unsw.edu.au>
2022-03-12 20:22:44 +1100

    Adding doc string

--- app/healthCheck.py
+++ app/healthCheck.py
@@ -2,6 +2,21 @@ from __init__ import startTime
 from datetime import datetime
 
 def healthCheckInfo():
+    '''
+    Returns information about the current state of the server, such as if the 
+    server is alive and how long the server has been running
+
+    Arguments:
+        None
+
+    Exceptions:
+        InputError  - None
+        AccessError - None
+
+    Return Value:
+        Returns {'alive': alive, 'serverUpTime': upTime}
+    '''
+
     upTime = datetime.now() - startTime
     alive = True
     return {'alive': alive, 'serverUpTime': upTime}
