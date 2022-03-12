d6a42070fa2efdaa956059703f0b737bf86c20be
Michelle Kaminski <z5362642@cse.unsw.edu.au>
2022-03-13 10:59:20 +1100

    tests: test for healthCheckInfo

--- tests/function/healthCheckInfo_test.py
+++ tests/function/healthCheckInfo_test.py
@@ -2,4 +2,6 @@ import pytest
 from app import healthCheck
 
 def test_health_check_info():
-    
+    info = healthCheck.healthCheckInfo()
+    assert info['alive'] == True
+    assert info['serverUpTime'] > 0
