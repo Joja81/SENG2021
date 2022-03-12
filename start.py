72cd084b42df97f23122615b2146106f0751de60
Joshua Smee <smeejosh81@gmail.com>
2022-03-13 10:44:29 +1100

    Added check for import of enviro

--- start.py
+++ start.py
@@ -1,8 +1,11 @@
+import os
 import config
-import enviro
 from app import init_app, email
 from flask import Flask
 import signal
+
+if 'ON_HEROKU' not in os.environ: # Checks if running on 
+    import enviro
 
 def graceful_exit(*args):
     email.exit()
@@ -11,5 +14,7 @@ def graceful_exit(*args):
 app = init_app()
 
 if __name__ == "__main__":
+    print(os.environ.get("ON_HEROKU"))
+    
     signal.signal(signal.SIGINT, graceful_exit)
     app.run(port = config.port, debug = True)
