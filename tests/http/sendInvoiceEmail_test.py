d7399315272a58e04023052be0a5e96b4846d577
Joshua Smee <smeejosh81@gmail.com>
2022-03-12 17:11:16 +1100

    Added invalid token test for sendInvoice

--- tests/http/sendInvoiceEmail_test.py
+++ tests/http/sendInvoiceEmail_test.py
@@ -3,6 +3,11 @@ import pytest
 import requests
 from config import url
 
+def invalid_auth():
+    file = {'file': open('./tests/files/AUInvoice.xml', 'rb')}
+    response = requests.post(f"{url}/sendInvoice", files=file, json = {'token' : "Hello"})
+    assert response.status_code == 403
+
 def test_basic():
     file = {'file': open('./tests/files/AUInvoice.xml', 'rb')}
     response = requests.post(f"{url}/sendInvoice", files=file)
