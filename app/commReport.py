6624408aa81502fa033ba6af6f4aead0acb1d9a0
Tay <iamtayleung@gmail.com>
2022-03-13 17:29:37 +1100

    feat: added error codes 4 and 5

--- app/commReport.py
+++ app/commReport.py
@@ -6,7 +6,8 @@ error_messages = {
     1: 'XML not found. ',
     2: 'Invoice file size over 10MB. ',
     3: 'Email is not of valid format. ',
-    4: 'Server not connected. '
+    4: 'Server not connected. ',
+    5: 'Server rejected all recipients (mail not sent). '
     }
 
 """
@@ -25,18 +26,18 @@ error_messages = {
         JSON communication report.
 
     """
-def communication_report(error_codes: list, time_sent: datetime, connected: bool):
+def communication_report(error_codes: list, time_sent: datetime):
     report_dict = {
-        "readable_message": '',
+        "sentMail": error_codes == [],  
+        "readable_errors": '',
         "xmlFound": 1 not in error_codes,
         "xmlRightSize": (1 and 2) not in error_codes,
         "emailValid": 3 not in error_codes,
-        "connectedToMail": connected,
-        "sentMail": error_codes == [],
+        "connectedToMail": 4 not in error_codes,
         "timeSent": time_sent.strftime("%m/%d/%Y, %H:%M:%S"),
         "timeTaken": (datetime.now() - time_sent).total_seconds()
     }
     for error_code in error_codes:
         if error_messages.get(error_code):
-            report_dict['readable_message'] += (error_messages.get(error_code))
+            report_dict['readable_errors'] += (error_messages.get(error_code))
     return json.dumps(report_dict)
