e33e4ea31207d45660bb7a97249b3ea4de3926a9
Tay <iamtayleung@gmail.com>
2022-03-13 16:35:13 +1100

    fix: minor error code number fix

--- app/commReport.py
+++ app/commReport.py
@@ -3,9 +3,10 @@ import json
 
 # dictionary of error code messages, can update as we need
 error_messages = {
-    1:'Email is not of valid format. ',
-    2:'Invoice file size over 10MB. ',
-    3:'Server not connected. '
+    1: 'XML not found. ',
+    2: 'Invoice file size over 10MB. ',
+    3: 'Email is not of valid format. ',
+    4: 'Server not connected. '
     }
 
 """
@@ -27,9 +28,9 @@ error_messages = {
 def communication_report(error_codes: list, time_sent: datetime, connected: bool):
     report_dict = {
         "readable_message": '',
-        "xmlFound": True,
-        "xmlRightSize": False if (2 in error_codes) else True,
-        "emailValid": False if (1 in error_codes) else True,
+        "xmlFound": False if (1 in error_codes) else True,
+        "xmlRightSize": False if ((1 or 2) in error_codes) else True,
+        "emailValid": False if (3 in error_codes) else True,
         "connectedToMail": connected,
         "sentMail": error_codes == [],
         "timeSent": time_sent.strftime("%m/%d/%Y, %H:%M:%S"),
@@ -37,5 +38,5 @@ def communication_report(error_codes: list, time_sent: datetime, connected: bool
     }
     for error_code in error_codes:
         if error_messages.get(error_code):
-            report_dict['errors'].append(error_messages.get(error_code))
+            report_dict['readable_message'].append(error_messages.get(error_code))
     return json.dumps(report_dict)
